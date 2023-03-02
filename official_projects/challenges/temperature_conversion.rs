2a17de85cc9552bd7bc03a9b7ae70a270e1a6243
Harilal <7802150+HarilalOP@users.noreply.github.com>
2023-03-02 21:12:09 +0200

    add script to convert temperature

---
+++ official_projects/challenges/temperature_conversion.rs
@@ -0,0 +1,6 @@
+// Convert temperatures between Fahrenheit and Celsius
+fn main() {
+    let temp_in_celsius: f32 = 32.0;
+    let temp_in_faherenheit = 1.8 * temp_in_celsius;
+    println!("{temp_in_celsius} celsius = {temp_in_faherenheit} Fahrenheit");
+}
