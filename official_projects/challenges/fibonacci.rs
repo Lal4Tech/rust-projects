60387e9a7d3576e6a491b9efcae17a5a90566141
Harilal <7802150+HarilalOP@users.noreply.github.com>
2023-03-04 14:55:10 +0200

    update fibonacci script

--- official_projects/challenges/fibonacci.rs
+++ official_projects/challenges/fibonacci.rs
@@ -1,6 +1,19 @@
 // Generate the nth Fibonacci number
+use std::io;
+
 fn main() {
-    let n = 5;
+    let mut n = String::new();
+
+    println!("Please input n for printing nth Fibonacci number");
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read line");
+
+    let n: u32 = n.trim().parse()
+        .expect("Please enter valid input");
+
+
     let mut a = 0;
     let mut b = 1;
     if n <= 0 {
